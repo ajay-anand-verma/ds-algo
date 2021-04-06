11d104a6bb11c11833323982a1352a64f464debd
Ajay Anand Verma <ajay.codifer@gmail.com>
2021-04-06 08:50:16 +0530

    Started work on linear data structures

--- linear/multi_field.cpp
+++ linear/multi_field.cpp
@@ -8,6 +8,8 @@ typedef struct {
 } team;
 
 bool comp(team a, team b){
+	// not a good approach for large number of multi-fiels 
+	// as nesting level will increase
 	if(a.solved==b.solved){
 		if(a.penalty==b.penalty)
 			return a.id<b.id;
@@ -17,17 +19,27 @@ bool comp(team a, team b){
 	return a.solved>b.solved;
 }
 
+bool comp2(team a, team b){
+	// No nesting a better way
+	if(a.solved != b.solved)
+		return a.solved > b.solved;
+	else if(a.penalty != b.penalty)
+		return a.penalty < b.penalty;
+	else
+		return a.id < b.id;
+}
+
 void solve(){
 	team ar[] =	{	{1, 3, 40},
-					{2, 2, 50},
+					{4, 2, 50},
 					{3, 3, 50},
-					{4, 2, 50}};
+					{2, 2, 50}};
 
 	for(auto x: ar)
 		cout<<x.id<<" "<<x.solved<<" "<<x.penalty<<endl;
 	
 	cout<<"\nSorted"<<endl;
-	sort(ar, ar+4, comp);
+	sort(ar, ar+4, comp2);
 	for(auto x: ar)
 		cout<<x.id<<" "<<x.solved<<" "<<x.penalty<<endl;
 }
